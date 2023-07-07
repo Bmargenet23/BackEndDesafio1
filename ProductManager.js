122a4475b2713401bec61b4804f6010d9bfbfc56
Brian Margenet <brianmargenet@gmail.com>
2023-07-07 00:12:17 -0300

    Segundo Desafio Backend

--- ProductManager.js
+++ ProductManager.js
@@ -1,32 +1,53 @@
-const Product = require("./Products.js");
+import fs from "fs/promises";
 
 class ProductManager {
-    products = [];
-    idNumber = 0;
-
-    addProduct(data) {
-        if (this.validateCode(data[4])) {
-            console.error("El código ya existe");
-            return;
-        }
-
-        if (this.validateObject(data)) {
-            console.error("Hay campos sin completar");
-            return;
-        }
-
-        const newProduct = {
-            code: this.nextCode++,
-            ...data
-        };
-
-        this.products.push(newProduct);
+    constructor() {
+        this.products = [];
     }
 
-    getProducts() {
-        console.table(this.products);
-        return this.products;
-    }
+    async addProduct(data) {
+        try {
+            const newProduct = {
+                id: this.products.length ? this.products[this.products.length - 1].id + 1 : 1,
+                title: data.title,
+                description: data.description,
+                price: data.price,
+                thumbnail: data.thumbnail,
+                code: data.code,
+                stock: data.stock,
+            }
+
+            if (this.validateCode(data)) {
+                console.error("El código ya existe");
+                return
+            }
+
+            if (this.validateObject(data)) {
+                console.error("Hay campos sin completar");
+                return
+            }
+
+            const file = await fs.readFile("./products.json", "utf8");
+            const products = JSON.parse(file);
+            products.push(newProduct);
+
+            await fs.writeFile("./products.json", JSON.stringify(products));
+            this.products.push(newProduct);
+            return newProduct;
+        } catch (e) {
+            console.log(e);
+        }
+
+    };
+
+    async getProducts() {
+        const file = await fs.readFile("./products.json", "utf8");
+        const products = JSON.parse(file);
+        console.log(products);
+        return products;
+        //   console.table(this.products);
+        //   return this.products;
+    };
 
     getProductById(id) {
         const product = this.products.find((p) => p.id === id);
@@ -38,38 +59,93 @@ class ProductManager {
         return product;
     }
 
-    generateID() {
-        if (this.idNumber === 0) {
-            this.idNumber = 1;
-            return;
-        }
-        this.idNumber++;
-    }
 
-    validateCode(code) {
-        if (this.products.find((p) => p.code === code)) {
+    validateCode(data) {
+        if (this.products.some((p) => p.code == data.code)) {
             return true;
         }
     }
 
     validateObject(data) {
-        if (data.length !== 6) return true;
-        data.map((element) => {
-            if (element === "" || element === undefined || element === null || element === 0) return true;
-        });
+        if (data.hasOwnProperty("title") &&
+            data.hasOwnProperty("description") &&
+            data.hasOwnProperty("price") &&
+            data.hasOwnProperty("thumbnail") &&
+            data.hasOwnProperty("code") &&
+            data.hasOwnProperty("stock")) {
+
+            for (const property in data) {
+                if (data[property] === "" ||
+                    property === undefined ||
+                    property === null ||
+                    property === 0)
+                    return true;
+            }
+        }
+        else { return true };
+    }
+
+    async saveProducts() {
+        try {
+          await fs.writeFile("./products.json", JSON.stringify(this.products));
+        } catch (error) {
+          console.log("Error al guardar los productos:", error);
+        }
+      }
+
+    async updateProduct(id, updatedFields) {
+        try {
+            const productIndex = this.products.findIndex((p) => p.id === id);
+            if (productIndex === -1) {
+                console.error("Not found");
+                return;
+            }
+
+            const updatedProduct = {
+                ...this.products[productIndex],
+                ...updatedFields,
+                id: this.products[productIndex].id, // me aseguro de que el ID no se modifique
+            };
+
+            this.products[productIndex] = updatedProduct;
+
+            await this.saveProducts();
+
+            return updatedProduct;
+        } catch (error) {
+            console.log("Error al actualizar el producto:", error);
+        }
+
+
+
+    } async deleteProduct(id) {
+        try {
+            const productIndex = this.products.findIndex((p) => p.id === id);
+            if (productIndex === -1) {
+                console.error("Not found");
+                return;
+            }
+
+            this.products.splice(productIndex, 1);
+
+            await this.saveProducts();
+
+            console.log("Producto eliminado correctamente");
+        } catch (error) {
+            console.log("Error al borrar el producto:", error);
+        }
     }
 }
 
+
 const test = new ProductManager();
 
-test.addProduct(["zapatillas", "nike rojas", 150, "http://", "1", 20]);
-test.addProduct(["botas", "zara negras", 130, "http://", "2", 20]);
-test.addProduct(["botas", "zara blancas", 110, "http://", "3", 30]);
-test.addProduct(["ojotas", "havaianas crudo", 110, "http://", "3", 20]);
-test.addProduct(["zapatillas", , 110, "http://", "4", 15,]);
-test.addProduct(["zapatillas", "adidas verde", 110, "5", ,]);
-test.getProducts();
-test.getProductById();
-test.getProductById(5);
-test.validateCode(4);
-test.validateObject(3);
+await test.addProduct({ title: "zapatillas", description: "zara negras", price: 1000, thumbnail: "http://a", code: 1000, stock: 50 });
+await test.addProduct({ title: "botas", description: "zara blancas", price: 1080, thumbnail: "http://dfg", code: 1001, stock: 55 });
+await test.addProduct({ title: "ojotas", description: "havaianas", price: 400, thumbnail: "http://df", code: 1001, stock: 50 });
+await test.addProduct({ title: "zapatillas", price: 1200, thumbnail: "http://qw", code: 1003, stock: 50 });
+await test.addProduct({ title: "zapatillas", description: "adidas verdes", price: 900, thumbnail: "http://qwe", code: 1012, stock: 50 });
+await test.getProducts();
+test.getProductById(2);
+await test.deleteProduct(2);
+await test.getProducts();
